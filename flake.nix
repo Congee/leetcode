ebbc2a4e6deb9bd7e9f5d9e2d2e03daee687054f
Congee <congeec@qq.com>
2021-04-28 12:46:10 -0400

    initial nix development environment setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "leetcode goes brrrrrrrrrrrrrrrrr";
+
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          clang
+        ];
+      };
+    }
+  );
+}

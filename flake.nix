6b348322dfd06643edec61ab05e4b590448e5e5c
Congee <congeec@qq.com>
2021-04-28 13:23:16 -0400

    chore: add clang-tools and bump clang_11

--- flake.nix
+++ flake.nix
@@ -10,12 +10,27 @@
   flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+      # XXX: the order of include matters
+      dotclangd = with pkgs; writeText ".clangd" ''
+        CompileFlags:
+          Add:
+            - --include-directory=${lib.makeSearchPathOutput "dev" "include/c++/v1" [ libcxx ]}
+            - --include-directory=${lib.makeSearchPathOutput "dev" "include" [ glibc ]}
+            - -std=c++17
+      '';
     in
     {
       devShell = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
-          clang
+          clang_11
+          clang-tools
         ];
+
+        # Why does not environment variables like C_INCLUDE_DIR work?
+        # FIXME: clean up the side effect
+        shellHook = with pkgs; ''
+          cp --force ${dotclangd} .clangd
+        '';
       };
     }
   );

4f4f6fac0cf8719463ee591dfffd0bbd986b63d2
Congee <congeec@qq.com>
2021-08-04 23:33:55 -0400

    fix: update flake.nix to avoid clang #include issue

--- flake.nix
+++ flake.nix
@@ -1,38 +1,48 @@
 {
-  description = "leetcode goes brrrrrrrrrrrrrrrrr";
+  description = "cracking the coding interview";
 
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      # XXX: the order of include matters
-      dotclangd = with pkgs; writeText ".clangd" ''
-        CompileFlags:
-          Add:
-            - --include-directory=${lib.makeSearchPathOutput "dev" "include/c++/v1" [ libcxx ]}
-            - --include-directory=${lib.makeSearchPathOutput "dev" "include" [ glibc ]}
-            - -std=c++17
-      '';
-    in
-    {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          clang_11
-          clang-tools
-        ];
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          llvm = pkgs.llvmPackages_latest;
+          lib = nixpkgs.lib;
 
-        # Why does not environment variables like C_INCLUDE_DIR work?
-        # FIXME: clean up the side effect
-        shellHook = with pkgs; ''
-          alias lc=leetcode
-          cp --force ${dotclangd} .clangd
-        '';
-      };
-    }
-  );
+        in
+          {
+            devShell = pkgs.mkShell {
+              nativeBuildInputs = [
+                # builder
+                # p.gnumake
+                # p.bear
+                pkgs.cmake
+                # debugger
+                llvm.lldb
+
+                # XXX: the order of include matters
+                pkgs.clang-tools
+                llvm.clang # clangd
+
+                pkgs.gtest
+              ];
+
+              buildInputs = [
+                # stdlib for cpp
+                llvm.libcxx
+              ];
+
+              # CXXFLAGS = "-std=c++17";
+              CPATH = builtins.concatStringsSep ":" [
+                (lib.makeSearchPathOutput "dev" "include" [ llvm.libcxx ])
+                (lib.makeSearchPath "resource-root/include" [ llvm.clang ])
+              ];
+            };
+          }
+    );
 }
